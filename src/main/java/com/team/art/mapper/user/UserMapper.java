a00293cd8e9fcbe7c70e43c4280e2850a1f969df
fengzidong <@10.16.52.40>
2018-11-14 12:12:14 +0800

    commit

---
+++ src/main/java/com/team/art/mapper/user/UserMapper.java
@@ -0,0 +1,17 @@
+package com.team.art.mapper.user;
+
+import com.team.art.entity.user.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}

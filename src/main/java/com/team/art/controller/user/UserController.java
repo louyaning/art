caa8223ff2f85e64a8d99c962f60a3549d3083a4
Liutengfeiyuanyaun <975445300@qq.com>
2018-11-14 13:59:23 +0800

    tf测试提交

---
+++ src/main/java/com/team/art/controller/user/UserController.java
@@ -0,0 +1,28 @@
+package com.team.art.controller.user;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+/**
+ * Created by YaNing on 2018/11/12.
+ */
+@Controller
+public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+    @RequestMapping("/test")
+    public String testJsp(){
+        logger.info("这是info日志");
+        return "test";
+    }
+    @ResponseBody
+    @RequestMapping("/testJson")
+    public String testJson(){
+        logger.info("这是info日志2");
+        return "hello,project is success";
+    }
+}

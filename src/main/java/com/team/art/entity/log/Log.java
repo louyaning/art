52fd44846eed4080a99052f53368d7fc65188332
Liutengfeiyuanyaun <975445300@qq.com>
2018-11-15 16:24:19 +0800

    新增实体类

---
+++ src/main/java/com/team/art/entity/log/Log.java
@@ -0,0 +1,45 @@
+package com.team.art.entity.log;
+
+import java.util.Date;
+
+public class Log {
+    private Long id;
+
+    private Long userId;
+
+    private String userName;
+
+    private Date loginTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public Date getLoginTime() {
+        return loginTime;
+    }
+
+    public void setLoginTime(Date loginTime) {
+        this.loginTime = loginTime;
+    }
+}
---
+++ src/main/java/com/team/art/entity/user/TeacherType.java
@@ -0,0 +1,55 @@
+package com.team.art.entity.user;
+
+import java.util.Date;
+
+public class TeacherType {
+    private Integer id;
+
+    private String type;
+
+    private Date createDatetime;
+
+    private Date modifyDatetime;
+
+    private Integer isDelete;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+
+    public Date getCreateDatetime() {
+        return createDatetime;
+    }
+
+    public void setCreateDatetime(Date createDatetime) {
+        this.createDatetime = createDatetime;
+    }
+
+    public Date getModifyDatetime() {
+        return modifyDatetime;
+    }
+
+    public void setModifyDatetime(Date modifyDatetime) {
+        this.modifyDatetime = modifyDatetime;
+    }
+
+    public Integer getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(Integer isDelete) {
+        this.isDelete = isDelete;
+    }
+}
